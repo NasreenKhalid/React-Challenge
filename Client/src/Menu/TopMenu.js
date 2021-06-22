65e1b7634d20ef15c75154193b4c383cd425ab85
NasreenKhalid <nasreen.1@live.com>
2021-06-22 13:05:38 +0300

    Update TopMenu.js

--- Client/src/Menu/TopMenu.js
+++ Client/src/Menu/TopMenu.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import logohere from '../Images/logohere.png'
+import debounce from 'lodash.debounce';
 
 function TopMenu(){
+    state = {
+    users: []
+}
+
+onChange = debounce(async (e,client) => {
+    console.log('Searching')
+    const res =  await client.query({
+        query: SEARCH_USERS_QUERY,
+        variables: {searchTerm: e.target.value}
+    });
+    this.setState({
+        users: res.data.users,
+    })
+},350)
+
     return (
         <div className="top-menu is-shadow-2">
             <div className="menu-logo-container">
@@ -14,6 +30,7 @@ function TopMenu(){
                     type="search"
                     name="name"
                     autoComplete="off"
+                    onChange={e=>this.onChange(e,client)}
                 />
             </div>
         </div>
